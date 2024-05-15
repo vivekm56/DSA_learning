df0f77320787b73301b20b5971b00b1a4ed196e8
vivekm56 <mudgalv56@gmail.com>
2024-05-15 23:22:27 +0530

    first

--- warmup/staircase/staircase.go
+++ warmup/staircase/staircase.go
@@ -1,22 +1,46 @@
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+//	func staircase(n int32) {
+//		for i := int32(0); i < n; i++ {
+//			row := ""
+//			for j := int32(0); j < n-i-1; j++ {
+//				row = fmt.Sprintf("%s ", row)
+//			}
+//			for j := int32(0); j <= i; j++ {
+//				row = fmt.Sprintf("%s#", row)
+//			}
+//			fmt.Println(row)
+//		}
+//	}
+//
+//	func main() {
+//		var n int32 = 8
+//		staircase(n)
+//	}
 package main
 
 import (
 	"fmt"
 )
 
-func staircase(n int32) {
-	for i := int32(0); i < n; i++ {
-		row := ""
-		for j := int32(0); j < n-i-1; j++ {
-			row = fmt.Sprintf("%s ", row)
+func staircasePattern(n int) {
+	for i := 0; i < n; i++ {
+		r := ""
+		for j := 0; j < n-i-1; j++ {
+			r += " "
 		}
-		for j := int32(0); j <= i; j++ {
-			row = fmt.Sprintf("%s#", row)
+		for j := 0; j <= i; j++ {
+			r += "A"
 		}
-		fmt.Println(row)
+		fmt.Println(r)
 	}
 }
+
 func main() {
-	var n int32 = 6
-	staircase(n)
+	n := 5
+	staircasePattern(n)
 }
